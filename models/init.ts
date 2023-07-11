18ad992c400c733a80cc7d5480d87ad6d591fee6
Mike Firpionn <mike.firpionn75@gmail.com>
2023-07-11 17:08:26 +0200

    add get and post

--- models/init.ts
+++ models/init.ts
@@ -1,7 +1,7 @@
 import {Movie} from '../models/movie'
 
 const create_tables = async () => {
-    await Movie.sync() // equal to CREATE TABLE IF NOT EXISTS
+    await Movie.sync({force:true}) // equal to CREATE TABLE IF NOT EXISTS
 
 }
 
