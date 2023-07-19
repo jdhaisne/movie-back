987fcceda7016c70e29dd2fac1c8a8e224e80a2f
Mike Firpionn <mike.firpionn75@gmail.com>
2023-07-19 16:49:32 +0200

    ??

--- controllers/CComment.ts
+++ controllers/CComment.ts
@@ -2,12 +2,11 @@ import {MComment} from '../models/MComment'
 import { v4 as uuid4 } from 'uuid';
 
 exports.postComment = async (req:any, res:any) => {
-    const comment = MComment.create({
-        Message : req.body.message,
-        UserId : req.body.userID,
-        TopicId : req.body.topicID,
+    const comment:any = MComment.create({
+        message : req.body.message,
+        userId : req.body.userID,
+        topicId : req.body.topicID,
     })
-    comment
     .then(() => res.status(201).json({ comment }))
     .catch((error:any) => res.status(400).json({ error}))
 }
@@ -25,11 +24,8 @@ exports.getAllComments = async (req:any, res:any, next:any) => {
   }
 
   exports.getOneComment =  async (req:any, res:any, next:any) => {
-<<<<<<< HEAD:controllers/comment.ts
-    Comment.findAll({where: {topicId: req.params.id}})
-=======
-    MComment.findAll({where: {id: req.params.id}})
->>>>>>> d244daffd09d3ba1dfe8338a367ee82696ec7174:controllers/CComment.ts
+
+    MComment.findAll({where: {topicId: req.params.id}})
     .then((comment:any) => {
         res.status(200).json(comment);
       })
