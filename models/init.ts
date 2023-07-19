5bf773d0ab77553f518592099670d9875e83c144
PaulineHavez <131375628+PaulineHavez@users.noreply.github.com>
2023-07-19 17:08:38 +0200

    rating

--- models/init.ts
+++ models/init.ts
@@ -2,12 +2,16 @@ import { Movie } from "../models/movie";
 import { Topic } from "../models/topic";
 import { Comment } from "../models/comment";
 import { User } from "./MUser";
+import { MLike } from "./MLike";
+import { MRating } from "./MRating";
 
 const create_tables = async () => {
   await Movie.sync();
   await Topic.sync();
   await Comment.sync();
   await User.sync();
+  await MLike.sync();
+  await MRating.sync();
 };
 
 create_tables();
