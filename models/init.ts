f1fc23f71c87b35e1271b2db3c6adf5d2f6e0049
jdhaisne <julien.dhaisne@gmail.com>
2023-07-20 10:22:33 +0200

    refacto init

--- models/init.ts
+++ models/init.ts
@@ -1,12 +1,12 @@
-import { Movie } from "../models/movie";
-import { Topic } from "../models/topic";
-import { Comment } from "../models/comment";
+import { MMovie } from "../models/MMovie";
+import { MTopic } from "../models/MTopic";
+import { MComment } from "../models/MComment";
 import { User } from "./MUser";
 
 const create_tables = async () => {
-  await Movie.sync();
-  await Topic.sync();
-  await Comment.sync();
+  await MMovie.sync();
+  await MTopic.sync();
+  await MComment.sync();
   await User.sync();
 };
 
