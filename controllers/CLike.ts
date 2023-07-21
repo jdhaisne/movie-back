27fa53709bd57f28fc3b7554941532a7f8907dbe
Mike Firpionn <mike.firpionn75@gmail.com>
2023-07-21 11:50:22 +0200

    rine ne s'est passeeé

--- controllers/CLike.ts
+++ controllers/CLike.ts
@@ -26,13 +26,6 @@ const getLikeById = async (req: any, res: any) => {
       res.status(500).send("An error occurred: ID needed");
       return;
     }
-    const like = await MLike.findByPk(id);
-    console.log(like);
-    res.send(like);
-  } catch (err) {
-    console.error("Error occurred:", err);
-    res.status(500).send("An error occurred");
-  }
 };
 
 const getLikesByUserId = async (req: any, res: any) => {
