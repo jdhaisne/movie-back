42359b125cc6a67597b4f754e5b69fe54d74012a
jdhaisne <julien.dhaisne@gmail.com>
2023-07-24 09:19:11 +0200

    uncommented topic route

--- controllers/CTopic.ts
+++ controllers/CTopic.ts
@@ -1,32 +1,32 @@
 import { MTopic } from "../models/MTopic";
 import { v4 as uuid4 } from "uuid";
 
-exports.postTopic = async (req: any, res: any, next: any) => {
-  const topic = MTopic.create({
-    title: req.body.title,
-    subject: req.body.subject,
-    movieId: req.params.id,
-    type: req.body.type,
-  });
-  console.log(topic);
-  topic
-    .then(() => res.status(201).json({ topic }))
-    .catch((error: any) => res.status(400).json({ error }));
-};
-
-// exports.postTopicWithUserId = async (req: any, res: any, next: any) => {
+// exports.postTopic = async (req: any, res: any, next: any) => {
 //   const topic = MTopic.create({
 //     title: req.body.title,
 //     subject: req.body.subject,
-//     movieId: req.params.movieId,
+//     movieId: req.params.id,
 //     type: req.body.type,
-//     userId: req.params.userId,
 //   });
 //   console.log(topic);
 //   topic
 //     .then(() => res.status(201).json({ topic }))
 //     .catch((error: any) => res.status(400).json({ error }));
 // };
+
+exports.postTopicWithUserId = async (req: any, res: any, next: any) => {
+  const topic = MTopic.create({
+    title: req.body.title,
+    subject: req.body.subject,
+    movieId: req.params.movieId,
+    type: req.body.type,
+    userId: req.params.userId,
+  });
+  console.log(topic);
+  topic
+    .then(() => res.status(201).json({ topic }))
+    .catch((error: any) => res.status(400).json({ error }));
+};
 
 exports.getAllTopics = async (req: any, res: any, next: any) => {
   MTopic.findAll()
--- routes/RTopic.ts
+++ routes/RTopic.ts
@@ -4,11 +4,11 @@ const topicCtrl = require("../controllers/CTopic");
 
 const router = express.Router();
 
-router.post("/createTopic/:id", topicCtrl.postTopic);
+// router.post("/createTopic/:id", topicCtrl.postTopic);
 router.get("/", topicCtrl.getAllTopics);
 router.get("/:id", topicCtrl.getOneTopic);
 router.get("/movie/:id", topicCtrl.getAllTopicsByMovieId);
-// router.post("/createTopic/:movieId/:userId", topicCtrl.postTopicWithUserId);
+router.post("/createTopic/:movieId/:userId", topicCtrl.postTopicWithUserId);
 router.get("/byUser/:userId", topicCtrl.getAllTopicsbyUserId);
 
 module.exports = router;
