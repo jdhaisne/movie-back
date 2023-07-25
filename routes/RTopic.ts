ed576416db09cb370bf4453f0975ff73977925b7
Mike Firpionn <mike.firpionn75@gmail.com>
2023-07-25 10:59:12 +0200

    ee

--- routes/RTopic.ts
+++ routes/RTopic.ts
@@ -9,6 +9,7 @@ router.get("/", topicCtrl.getAllTopics);
 router.get("/:id", topicCtrl.getOneTopic);
 router.get("/movie/:id", topicCtrl.getAllTopicsByMovieId);
 router.post("/createTopic/:movieId/:userId", topicCtrl.postTopicWithUserId);
+router.post("/createTopic/:id", topicCtrl.postTopic);
 router.get("/byUser/:userId", topicCtrl.getAllTopicsbyUserId);
 
 module.exports = router;
